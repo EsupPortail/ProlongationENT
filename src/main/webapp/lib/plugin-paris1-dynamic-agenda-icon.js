7090d491242a9e50125bd699ec2f48fe0c629f1e
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-01-31 13:27:45 +0100

    paris1-dynamic-agenda-icon: export function for re-use in accueil ENT + move class "icon" directly in <svg>

--- src/main/webapp/lib/plugin-paris1-dynamic-agenda-icon.js
+++ src/main/webapp/lib/plugin-paris1-dynamic-agenda-icon.js
@@ -1,14 +1,13 @@
 (function () {
 
-    function dynamic_agenda_icon() {
-        var img = h.simpleQuerySelector("#pE-header .pE-button img[src$='agenda.svg'");
+    var dynamic_agenda_icon = pE['dynamic_agenda_icon'] = function (selector) {
+        var img = h.simpleQuerySelector(selector);
         if (!img) return;
         
         var e = document.createElement("span");
         e.style = "display: inline-block";
-        e.className = "icon";
         e.innerHTML = ""
- + '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40px" height="40px" viewBox="-453.618 -905.311 282.232 282.232">'
+ + '<svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40px" height="40px" viewBox="-453.618 -905.311 282.232 282.232">'
  + '<circle fill="#00326E" cx="-312.503" cy="-764.195" r="141.116"/>'
  + '<path opacity="0.1" fill="#FFFFFF" d="M-185.512-825.808c9.05,18.617,14.126,39.522,14.126,61.612   c0,77.938-63.178,141.117-141.111,141.117c-77.932,0-141.108-63.18-141.108-141.117c0-19.484,3.949-38.045,11.089-54.928"/>'
  + '<path fill="#FFFFFF" d="M-448.742-821.5v-8.232h8.232v8.232H-448.742z"/>'
@@ -32,10 +31,10 @@
  + '<text id="day" x="-314" y="-700" font-family="Arial" font-size="117" fill="#FFFFFF" text-anchor="middle">' + new Date().getDate() + '</text>'
  + '</svg>';
         img.parentElement.replaceChild(e, img);
-    }
+    };
 
     var plugin = {
-        post_header_add: dynamic_agenda_icon
+        post_header_add: function () { dynamic_agenda_icon("#pE-header .pE-button img[src$='agenda.svg'"); }
     };
     pE.plugins.push(plugin);   
 })();
