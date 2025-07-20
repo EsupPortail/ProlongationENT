3139b29594d1f35c0999ed037f4e2a45558bcc22
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2025-07-20 13:04:41 +0200

    chore: rely on EsupUserApps /login to redirect to cas_login_url (useful for next commit)

--- src/main/webapp/lib/plugin-delegateAuth.js
+++ src/main/webapp/lib/plugin-delegateAuth.js
@@ -3,7 +3,7 @@
         onNotLogged: function() {
             if (args.delegateAuth) {
                 var url = (args.esupUserApps_url || pE.CONF.esupUserApps_url) + "/login?target=" + encodeURIComponent(document.location.href);
-                document.location.href = pE.CONF.cas_login_url + "?service=" + encodeURIComponent(url);
+                document.location.href = url;
             }
         },
     };
