ce610445380ad49261622c2889ac3d3c1a5bdbbb
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2026-01-23 17:27:49 +0100

    chore: migration javax->jakarta
    obtenu avec :
    
    git grep -l javax | xargs sed -i 's/javax/jakarta/'
    mvn versions:use-latest-versions -Dincludes=jakarta.servlet:jakarta.servlet-api

--- src/main/java/prolongationENT/LoaderJs.java
+++ src/main/java/prolongationENT/LoaderJs.java
@@ -2,9 +2,9 @@ package prolongationENT;
 
 import java.io.IOException;
 import java.util.Map;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.LoggerFactory;
 
 import static prolongationENT.Utils.*;
